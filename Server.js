45cd011c04e32f7bf30e3bb0ede7aa4c1e972150
Mark Glasman <glasman.mark@gmail.com>
2023-11-27 17:00:26 -0500

    intial commit

---
+++ Server.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+const app = express();
+
+const PORT = 8080
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });   
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+const petsData = require('./Data.js');
+
+app.get('/api/v1/pets', function (req, res) {
+  const petNames = petsData.map((pet) => pet.name);
+ 
+  res.send(petNames)
+})
+
+app.get('/api/v1/pets/:name', function (req, res) {
+  const petName = req.params.name;
+  const foundPet = petsData.find((pet) => pet.name === petName);
+  res.send(foundPet)
+})
